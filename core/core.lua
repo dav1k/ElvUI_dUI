be8aa3471250beb7a7fffc0f3755108bf05d2256
dav1k <dave.klausz@icloud.com>
2017-07-15 06:44:34 -0700

    Included global string.match

--- core/core.lua
+++ core/core.lua
@@ -5,7 +5,7 @@ local dUI = E:GetModule('dUI')	-- Import dUI Core
 local select, tonumber, assert, type, unpack, pairs = select, tonumber, assert, type, unpack, pairs
 local tinsert, tremove = tinsert, tremove
 local atan2, modf, ceil, floor, abs, sqrt, mod = math.atan2, math.modf, math.ceil, math.floor, math.abs, math.sqrt, mod
-local format, sub, upper, split, utf8sub = string.format, string.sub, string.upper, string.split, string.utf8sub
+local format, sub, match, upper, split, utf8sub = string.format, string.sub, string.match, string.upper, string.split, string.utf8sub
 
 -- Format number to a specific manitude of precision
 local function FormatToDecimalsPlaces(num, digits)
